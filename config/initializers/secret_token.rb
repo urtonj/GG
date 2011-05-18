2b7e64f7af31626752a1e5a5dca85ac269b58559
Jason Urton <jason.urton@gmail.com>
2011-05-18 15:34:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GG::Application.config.secret_token = '3144a94c29fe91d4647e5b733299adb08d5df5a9514fc0f7c25ceac355b66d82d9dc3f3c358b9ff518bc74f5f5301ccc0c6451cb8702140bb227b8b50cc9001c'
